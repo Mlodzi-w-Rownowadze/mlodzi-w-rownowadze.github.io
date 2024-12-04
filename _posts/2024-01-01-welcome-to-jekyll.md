7dd2237773f39a1c63122700623f53acf73b8f71
lodyczekoladowe <lodyczekoladowe@git.com>
2024-12-04 18:49:50 +0100

    font change and added working

--- _posts/2024-01-01-welcome-to-jekyll.md
+++ _posts/2024-01-01-welcome-to-jekyll.md
@@ -4,4 +4,8 @@ title:  "Welcome to Jekyll!"
 date:   2024-01-01 00:00:00 +0000
 categories: jekyll update
 ---
-You’ll find this post in your `_posts` directory. Go ahead and edit it and re-build the site to see your changes.
+
+
+## You’ll find this post in your `_posts` directory. Go ahead and edit it and re-build the site to see your changes.
+
+![Image](/assets/images/logo-navbar.png "a title")
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,108 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleButton = document.getElementById('toggleMode');
+/*!
+ * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
+ * Copyright 2011-2024 The Bootstrap Authors
+ * Licensed under the Creative Commons Attribution 3.0 Unported License.
+ */
 
-    toggleButton.addEventListener('click', function() {
-        if (document.body.classList.contains('light-mode')) {
-            document.body.classList.remove('light-mode');
-            document.body.classList.add('dark-mode');
-            localStorage.setItem('theme', 'dark-mode');
-            console.log('Current theme:', document.body.classList);
-        } else {
-            document.body.classList.remove('dark-mode');
-            document.body.classList.add('light-mode');
-            localStorage.setItem('theme', 'light-mode');
-            console.log('Current theme:', document.body.classList);
+(() => {
+    'use strict'
+
+    const getStoredTheme = () => localStorage.getItem('theme')
+    const setStoredTheme = theme => localStorage.setItem('theme', theme)
+
+    const getPreferredTheme = () => {
+        const storedTheme = getStoredTheme()
+        if (storedTheme) {
+            return storedTheme
         }
-    });
-});
+
+        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    }
+
+    const setTheme = theme => {
+        if (theme === 'auto') {
+            document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
+        } else {
+            document.documentElement.setAttribute('data-bs-theme', theme)
+        }
+    }
+
+    const updateIconClasses = (theme) => {
+        const themeSwitcherIcon = document.querySelector('#bd-theme i');
+        if (theme === 'dark') {
+            themeSwitcherIcon.className = 'bi bi-moon-stars-fill';
+        } else if (theme === 'light') {
+            themeSwitcherIcon.className = 'bi bi-sun-fill';
+        } else {
+            themeSwitcherIcon.className = 'bi bi-circle-half';
+        }
+    }
+
+    setTheme(getPreferredTheme())
+
+    const showActiveTheme = (theme, focus = false) => {
+        const themeSwitcher = document.querySelector('#bd-theme');
+
+        if (!themeSwitcher) {
+            console.error('Element with id "bd-theme" not found.');
+            return;
+        }
+
+        const themeSwitcherText = document.querySelector('#bd-theme-text');
+        if (!themeSwitcherText) {
+            console.error('Element with id "bd-theme-text" not found.');
+            return;
+        }
+
+        const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`);
+        if (!btnToActive) {
+            console.error(`Element with data-bs-theme-value="${theme}" not found.`);
+            return;
+        }
+
+        document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
+            element.classList.remove('active');
+            element.setAttribute('aria-pressed', 'false');
+            const checkIcon = element.querySelector('.bi-check2');
+            if (checkIcon) {
+                checkIcon.classList.add('d-none');
+            }
+        });
+
+        btnToActive.classList.add('active');
+        btnToActive.setAttribute('aria-pressed', 'true');
+        const checkIcon = btnToActive.querySelector('.bi-check2');
+        if (checkIcon) {
+            checkIcon.classList.remove('d-none');
+        }
+        const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`;
+        themeSwitcher.setAttribute('aria-label', themeSwitcherLabel);
+
+        updateIconClasses(theme);
+
+        if (focus) {
+            themeSwitcher.focus();
+        }
+    };
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+        const storedTheme = getStoredTheme()
+        if (storedTheme !== 'light' && storedTheme !== 'dark') {
+            setTheme(getPreferredTheme())
+        }
+    })
+
+    window.addEventListener('DOMContentLoaded', () => {
+        showActiveTheme(getPreferredTheme())
+
+        document.querySelectorAll('[data-bs-theme-value]')
+            .forEach(toggle => {
+                toggle.addEventListener('click', () => {
+                    const theme = toggle.getAttribute('data-bs-theme-value')
+                    setStoredTheme(theme)
+                    setTheme(theme)
+                    showActiveTheme(theme, true)
+                })
+            })
+    })
+})()
